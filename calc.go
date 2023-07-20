300cb92ed93b9b4115966b17d4fa2b38bb4d3241
Jaiprakash <73591435+jaiprakash-singh@users.noreply.github.com>
2023-07-20 09:49:32 +0530

    Added Calc implementation

---
+++ calc.go
@@ -0,0 +1,41 @@
+package gocalculator
+
+import "errors"
+
+func Add(nums ...float64) (res float64) {
+	for _, n := range nums {
+		res += n
+	}
+
+	return
+}
+
+func Substract(num0 float64, nums ...float64) float64 {
+	var allNum float64
+	for _, n := range nums {
+		allNum += n
+	}
+
+	return num0 - allNum
+}
+
+func Multiply(nums ...float64) (res float64) {
+	for _, n := range nums {
+		res *= n
+	}
+
+	return
+}
+
+func Divide(num0 float64, nums ...float64) (float64, error) {
+	var allNum float64
+	for _, n := range nums {
+		allNum *= n
+	}
+
+	if allNum == 0 {
+		return 0, errors.New("divisor cannot be zero")
+	}
+
+	return num0 / allNum, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jaiprakash-singh/gocalculator
+
+go 1.20
